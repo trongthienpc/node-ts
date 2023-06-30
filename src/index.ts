be5ba5cabf45017effd7c38db77b8b08bb321b21
thientt <thientt.bdst@gmail.com>
2023-06-30 22:41:04 +0700

    initial-branch

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-const message: string = "Hello, TypeScript 1!";
+export const message: string = "Hello, TypeScript!";
 console.log(message);
---
+++ src/test/index.test.ts
@@ -0,0 +1,22 @@
+const originalLog = console.log; // Save the original console.log function
+
+// Mock console.log to capture its output
+let consoleOutput: string[] = [];
+const mockedLog = (output: string) => consoleOutput.push(output);
+console.log = mockedLog;
+
+// Import the code to be tested
+import { message } from "../index";
+
+describe("Testing TypeScript code", () => {
+  test("Console log message", () => {
+    expect(message).toBe("Hello, TypeScript!"); // Ensure the message is correct
+    expect(consoleOutput.length).toBe(1); // Ensure console.log was called once
+    expect(consoleOutput[0]).toBe("Hello, TypeScript!"); // Ensure console.log captured the expected output
+  });
+});
+
+// Restore the original console.log function after all tests have finished
+afterAll(() => {
+  console.log = originalLog;
+});
